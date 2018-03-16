6b342b82692f87e5d347e33f9c4ea10a06531a74
Michael Li <60745137>
2018-03-16 10:14:30 -0700

    Initial commit

---
+++ app/src/main/java/com/example/michaelli/ihopethisworks/Cust2.java
@@ -0,0 +1,9 @@
+package com.example.michaelli.ihopethisworks;
+
+/**
+ * Created by 60745137 on 2/23/2018.
+ */
+
+public class Cust2 {
+
+}
---
+++ app/src/main/java/com/example/michaelli/ihopethisworks/SingleViewActivity.java
@@ -0,0 +1,71 @@
+package com.example.michaelli.ihopethisworks;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SingleViewActivity extends AppCompatActivity {
+
+    private static final String TAG = "SingleViewActivity";
+
+    private SectionsPageAdapter mSectionsPageAdapter;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_view);
+        Intent i = getIntent();
+        // Selected image id
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+
+
+
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+        ImageView imageView = (ImageView) findViewById(R.id.SingleView);
+        TextView tv = (TextView) findViewById(R.id.text);
+        TextView tv2 = (TextView) findViewById(R.id.text2);
+        imageView.setImageResource(imageAdapter.mThumbIds[position]);
+        tv.setText(imageAdapter.two[position]);
+        tv2.setText(imageAdapter.three[position]);
+
+
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "TAB1");
+        adapter.addFragment(new Tab2Fragment(), "TAB2");
+        adapter.addFragment(new Tab3Fragment(), "TAB3");
+        viewPager.setAdapter(adapter);
+
+    }
+
+}
